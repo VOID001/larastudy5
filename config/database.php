fc28019a1bdf8e303a11c706780c07457e3ac5b6
Jianqiu Zhang <zhangjianqiu_133@yeah.net>
2015-11-17 16:50:56 +0800

    config/database: Update the app Configuration
    Update database configuration , use mySQL for the default PDO Driver

--- config/database.php
+++ config/database.php
@@ -26,7 +26,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    //'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'mysql',
 
     /*
     |--------------------------------------------------------------------------
@@ -54,9 +55,9 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
+            'host'      => env('DB_HOST', 'localhost:3306'),
+            'database'  => env('DB_DATABASE', 'laravel5'),
+            'username'  => env('DB_USERNAME', 'laravel5'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
